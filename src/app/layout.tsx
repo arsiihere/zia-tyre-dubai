20d50659b15a515d9f40e05a19e875815e9e2fde
Arslan Ahmed Arif <arslanahmedarif@Arslans-MacBook-Air.local>
2025-12-23 20:13:35 +0500

    feat: add google ads script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,24 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="gtm" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NRXQ3WRP');`}
+        </Script>
+      </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NRXQ3WRP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>{children}</Providers>
         <Script id="org-jsonld" type="application/ld+json" strategy="afterInteractive">
           {JSON.stringify(organizationJsonLd)}
