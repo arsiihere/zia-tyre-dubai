1f35bc8da496005cd50d4703ca7bcdcabc244a70
Arslan Ahmed Arif <arslanahmedarif@Arslans-MacBook-Air.local>
2025-12-26 12:21:09 +0500

    feat: add global site tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NRXQ3WRP');`}
         </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17818866540"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-ads" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-17818866540');`}
+        </Script>
       </head>
       <body className="antialiased">
         <noscript>
