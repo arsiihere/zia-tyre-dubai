24bb3f6347fc96a2e00c5fd84c9ef31ba28a1f41
Arslan Ahmed Arif <arslanahmedarif@Arslans-MacBook-Air.local>
2026-01-11 01:39:36 +0500

    feat: add new ads links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,27 +79,38 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NRXQ3WRP');`}
+})(window,document,'script','dataLayer','GTM-55R2CR9M');`}
         </Script>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17818866540"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17835138119"
           strategy="afterInteractive"
         />
         <Script id="gtag-ads" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'AW-17818866540');`}
+gtag('config', 'AW-17835138119');`}
+        </Script>
+        <Script id="clickcease" strategy="afterInteractive">
+          {`var script = document.createElement('script');
+script.async = true; script.type = 'text/javascript';
+var target = 'https://www.clickcease.com/monitor/stat.js';
+script.src = target;var elem = document.head;elem.appendChild(script);`}
         </Script>
       </head>
       <body className="antialiased">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NRXQ3WRP"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-55R2CR9M"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
+        </noscript>
+        <noscript>
+          <a href="https://www.clickcease.com" rel="nofollow">
+            <img src="https://monitor.clickcease.com" alt="ClickCease" />
+          </a>
         </noscript>
         <Providers>{children}</Providers>
         <Script id="org-jsonld" type="application/ld+json" strategy="afterInteractive">
